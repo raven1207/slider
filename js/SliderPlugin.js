f7057e0bff8f39ca4d96d0d6a74d868471290843
richardgong1987 <909253305@qq.com>
2016-08-07 15:12:42 +0800

    lazada test

--- js/SliderPlugin.js
+++ js/SliderPlugin.js
@@ -3,6 +3,10 @@
  */
 (function ($) {
 
+    /**
+     * 检测是否支持css3的工具方法,如果有,那么,就用css3实现slider效果。
+     * @returns {*}
+     */
     function transitionEnd() {
         var el = document.createElement('div')
         var transEndEventNames = {
@@ -21,6 +25,11 @@
         return false
     }
 
+    /**
+     * 用于模拟动画结束,而自定义事件
+     * @param duration
+     * @returns {jQuery}
+     */
     $.fn.emulateTransitionEnd = function (duration) {
         var called = false
         var $el = this
@@ -34,11 +43,16 @@
         return this
     }
 
-
+    /**
+     * 导出工具方法
+     * @type {any}
+     */
     $.support.transition = transitionEnd()
 
-    if (!$.support.transition) return
-
+    /**
+     * 把自定义事件放到jquery对象里,以方便在各个地方可以方便使用
+     * @type {{bindType: (any), delegateType: (any), handle: $.event.special.mysliderTransitionEnd.handle}}
+     */
     $.event.special.mysliderTransitionEnd = {
         bindType: $.support.transition.end,
         delegateType: $.support.transition.end,
@@ -47,7 +61,12 @@
         }
     }
 
-
+    /**
+     * slider插件构造函数
+     * @param element
+     * @param options
+     * @constructor
+     */
     var SliderContructor = function (element, options) {
         this.$element = $(element)
         this.$indicators = this.$element.find('.myslider-indicators')
@@ -68,6 +87,10 @@
 
     SliderContructor.TRANSITION_DURATION = 600
 
+    /**
+     * 默认slider参数
+     * @type {{interval: number, pause: string, wrap: boolean, keyboard: boolean}}
+     */
     SliderContructor.DEFAULTS = {
         interval: 5000,
         pause: 'hover',
@@ -75,7 +98,14 @@
         keyboard: true
     }
 
+    /**
+     *检测键盘事件,39表示按command键(mac机器里)。39表示按向左键
+     * @param e
+     */
     SliderContructor.prototype.keydown = function (e) {
+        /**
+         * 如果用户在input/textrea而出发的键盘事件,我们不作响应
+         */
         if (/input|textarea/i.test(e.target.tagName)) return
         switch (e.which) {
             case 37:
@@ -91,13 +121,22 @@
         e.preventDefault()
     }
 
+    /**
+     * slider轮询主体方法
+     * @param e
+     * @returns {SliderContructor}
+     */
     SliderContructor.prototype.cycle = function (e) {
         e || (this.paused = false)
 
+        /**
+         * 先清除之前的setInterval以确保调用队列可靠稳定
+         */
         this.interval && clearInterval(this.interval)
 
         this.options.interval
         && !this.paused
+
         && (this.interval = setInterval($.proxy(this.next, this), this.options.interval))
 
         return this
@@ -107,7 +146,12 @@
         this.$items = item.parent().children('.item')
         return this.$items.index(item || this.$active)
     }
-
+    /**
+     * 判断当前slider的动画方向后,再获取下一个应该活动的元素
+     * @param direction
+     * @param active
+     * @returns {*}
+     */
     SliderContructor.prototype.getItemForDirection = function (direction, active) {
         var activeIndex = this.getItemIndex(active)
         var willWrap = (direction == 'prev' && activeIndex === 0)
@@ -132,6 +176,11 @@
         return this.slide(pos > activeIndex ? 'next' : 'prev', this.$items.eq(pos))
     }
 
+    /**
+     * 动画停止方法
+     * @param e
+     * @returns {SliderContructor}
+     */
     SliderContructor.prototype.pause = function (e) {
         e || (this.paused = true)
 
@@ -145,16 +194,26 @@
         return this
     }
 
+    /**
+     * 下了个动画接口
+     */
     SliderContructor.prototype.next = function () {
         if (this.sliding) return
         return this.slide('next')
     }
-
+    /**
+     * 上一个动画接口
+     */
     SliderContructor.prototype.prev = function () {
         if (this.sliding) return
         return this.slide('prev')
     }
-
+    /**
+     * 动画的表现层的具体操作
+     * @param type
+     * @param next
+     * @returns {*}
+     */
     SliderContructor.prototype.slide = function (type, next) {
         var $active = this.$element.find('.item.active')
         var $next = next || this.getItemForDirection(type, $active)
@@ -212,7 +271,16 @@
     }
 
 
+    /**
+     * 插件业务入口
+     * @param option
+     * @returns {*}
+     * @constructor
+     */
     function SliderPlugin(option) {
+        /**
+         * this.each主要是为了实现在同一个页面上,多处调用插件
+         */
         return this.each(function () {
             var $this = $(this)
             var data = $this.data('myslider')
@@ -230,6 +298,10 @@
         });
     }
 
+    /**
+     * 点击左右方向键事件的,处理业务接口
+     * @param e
+     */
     function clickHandler(e) {
         var href
         var $this = $(this)
@@ -245,13 +317,23 @@
             $target.data('myslider').to(slideIndex)
         }
 
-        e.preventDefault()
+        e.preventDefault();
     }
+
+    /**
+     * 导出插件myslider到jquery上
+     * @type {SliderPlugin}
+     */
 
     $.fn.myslider = SliderPlugin
 
 }(jQuery));
 
+
+
+/**
+ * 调用插件实例
+ */
 $('.myslider').myslider();
 
 
